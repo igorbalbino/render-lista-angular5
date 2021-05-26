05583a5d7e45e5c8ca849cfc60ca87fca02bbe9b
Igor do E. Santo <igorbalbinox1@hotmail.com>
2021-05-26 20:27:29 -0300

    lista simples renderizada

---
+++ render-car-list/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import {CarListComponent} from './components/car-list/car-list.component'
+
+const routes: Routes = [
+  {
+    path: 'carList',
+    component: CarListComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
